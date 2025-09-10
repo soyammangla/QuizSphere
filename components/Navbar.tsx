ef57c093b556dcdb7babaf7ac18f12190ee275f5
soyammangla <soyammangla15@gmail.com>
2025-09-10 23:11:08 +0530

    msg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,23 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-black shadow-md mx-30">
-      <h1 className="text-xl font-bold">QuizSphere</h1>
+      <h1 className="text-xl font-bold text-white">QuizSphere</h1>
 
       <div className="flex space-x-10 ml-180">
-        <a href="/" className="text-white-700 hover:text-blue-600">
+        <Link href="/" className="text-white hover:text-blue-600">
           Home
-        </a>
-        <a href="/quiz" className="text-white-700 hover:text-blue-600">
+        </Link>
+        <Link href="/quiz" className="text-white hover:text-blue-600">
           Quiz
-        </a>
-        <a href="/about" className="text-white-700 hover:text-blue-600">
+        </Link>
+        <Link href="/about" className="text-white hover:text-blue-600">
           About
-        </a>
-        <a href="/profile" className="text-white-700 hover:text-blue-600">
+        </Link>
+        <Link href="/profile" className="text-white hover:text-blue-600">
           Profile
-        </a>
+        </Link>
       </div>
 
       <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
