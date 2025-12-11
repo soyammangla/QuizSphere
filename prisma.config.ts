7daf3597a724eb421b207f37af03fb169a378828
soyammangla <soyammangla15@gmail.com>
2025-12-11 18:25:08 +0530

    chore: update devDependencies and comment out Prisma configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,14 @@
-import "dotenv/config";
+// import "dotenv/config";
 
+// export default {
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: process.env.DATABASE_URL,
+//   },
+// };
 export default {
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
 };
